870521788a39f5cb6ab7f61c2d53cc58524d2f3e
Kaustubh Yeul <kaustubhyeul@gmail.com>
2025-11-06 11:59:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
+let list = document.getElementById("infi-list");
+
+let itemCount = 10;
+
+function addItems(num) {
+for(let i=1; i<= num; i++) {
+let li = document.createElement("li");
+li.textContent = "Item " + (list.children.length + 1);
+list.appendChild(li);
+}
+}
+
+addItems(itemCount);
+
+list.addEventListener("scroll", function() {
+if(list.scrollTop + list.clientHeight >= list.scrollHeight-5) {
+addItems(2);
+}
+})
+
+
 
